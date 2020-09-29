10bd1cc84766e8330867a1f1552d1788e348378b
Nazmul Hasan <nazmulimm@gmail.com>
2020-09-29 19:55:58 +0600

    final update

---
+++ maijdee-pouro-park/app-files/data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_e3451",
+      "name": "IMG_E3451",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4596777261433544,
+        "pitch": 0.033158026939215546,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7711425486123176,
+          "pitch": 0.1213344488141388,
+          "rotation": 7.0685834705770345,
+          "target": "1-img_e3452"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.435055936472586,
+          "pitch": 0.08806421205647297,
+          "title": "Maijdee Pouro Park",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-img_e3452",
+      "name": "IMG_E3452",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.306697777086944,
+          "pitch": 0.2578039810838515,
+          "rotation": 0.7853981633974483,
+          "target": "0-img_e3451"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9559648714200062,
+          "pitch": 0.0821873801219084,
+          "title": "Maijdee Pouro Park",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Maijdee Pouro Park",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
